89f091f6b9b6837555c6bed4c40261f8cdcb33b2
Mokány Ádám <mokanyadam@gmail.com>
2022-11-12 19:15:07 +0100

    - useRouteMatch

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,28 +1,43 @@
 import { Fragment } from "react";
-import { Route, useParams } from "react-router";
-import Comments from '../components/comments/Comments';
-import HighlightedQuote from '../components/quotes/HighlightedQuote'
+import { Route, useParams, useRouteMatch } from "react-router";
+import { Link } from "react-router-dom";
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 const DUMMY_QUOTES = [
-  {id: 'q1', author: 'Adam', text: 'Learning React is fun!'},
-  {id: 'q2', author: 'Adi', text: 'Learning React is great!'}
-]
+  { id: "q1", author: "Adam", text: "Learning React is fun!" },
+  { id: "q2", author: "Adi", text: "Learning React is great!" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+  const match = useRouteMatch();
+  console.log(match)
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
-const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
-
-if(!quote){
-  return <p>No quote found!</p>
-}
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
 
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author}></HighlightedQuote>
-      <Route path={`/quotes/${params.quoteId}/comments`}></Route>   {/*Ez egy dinamikus útvonal, ami így néz ki: /quotes/Ennek az oldalnak az Id-ja/comments(amit itt meg akarok jeleníteni.)
+      <HighlightedQuote
+        text={quote.text}
+        author={quote.author}
+      ></HighlightedQuote>
+      <Route path={`${match.path}`} exact>
+        {" "}
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
+        <Comments></Comments>
+      </Route>{" "}
+      {/*Ez egy dinamikus útvonal, ami így néz ki: /quotes/Ennek az oldalnak az Id-ja/comments(amit itt meg akarok jeleníteni.)
       Ezt egyébként nem dinamikusan is meg lehet adni: path='/quotes/:quoteId/comments' */}
-      <Comments></Comments>
     </Fragment>
   );
 };
